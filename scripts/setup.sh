1c448e18229b310c3dd52b897657d91bcfb72c97
Qubetzl <56742499+qubetzl@users.noreply.github.com>
2022-08-17 01:01:27 +0300

    Fixed syntax problem in 'setup.sh'

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [ ! command -v pip3 &> /dev/null]
+if [ ! command -v pip3 &> /dev/null ]
 then
     apt-get install -y python3-pip
 fi
@@ -14,7 +14,7 @@ fi
 [ ! -d "venv" ] && python3 -m venv venv
 
 source venv/bin/activate
-if [ ! command -v ansible &> /dev/null]
+if [ ! command -v ansible &> /dev/null ]
 then
     pip3 install ansible
 fi
