646908cc22e66c82f360369674bdd3798d534ad3
Qubetzl <56742499+qubetzl@users.noreply.github.com>
2022-08-17 00:59:12 +0300

    Privilege escalate in 'bootstrap.sh' only when necessary

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-apt-get update
-apt-get install -y git
+if [ ! command -v git &> /dev/null ]
+then
+    sudo apt-get update && sudo apt-get install -y git
+fi
 
 [ ! -d ~/ansible-dev-pc ] && git clone https://github.com/qubetzl/ansible-dev-pc.git ~/ansible-dev-pc
 pushd ~/ansible-dev-pc
